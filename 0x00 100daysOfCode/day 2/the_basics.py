ba1e6373133b22c3f25709bc086b726f2a26dab6
ana thayna <anathaynafranca@gmail.com>
2023-07-28 13:14:46 -0300

    :computer: development(dsa): adding map filter lambda examples

--- 0x00 100daysOfCode/day 2/the_basics.py
+++ 0x00 100daysOfCode/day 2/the_basics.py
@@ -28,11 +28,38 @@ print("### map, filter, lambda ###")
 
 items = [1, 2, 3, 4, 5]
 
-cubes = list(map(lambda x: x**3, items))
+# in multiple arguments, map() returns a list of tuples containing the corresponding items from all iterables
+cubes = list(map(lambda x: x**3, items)) # map(funcao, sequencia) applies to every member of iterable and returns the result
 print(cubes)
-print((lambda x: x**2)(5))
+print((lambda x: x**2)(5)) # inline function
 print((lambda x, y: x*y)(3, 4))
 
 number_list = range(-10, 10)
-less_than_five = list(filter(lambda x: x < 5, number_list))
-print(less_than_five)
+less_than_five = list(filter(lambda x: x < 5, number_list)) # takes a function returning True or False and applies it to a sequence
+print(less_than_five) # returning a list of only those members of the sequence for which the function returned True
+
+x = [2, 3, 4, 5, 6]
+y = []
+
+for v in x:
+    if v % 2:
+        y += [v*5]
+
+print(y)
+
+y = list(map(lambda v: v*5, filter(lambda u: u % 2, x))) # using lambda and map you can have two for loops in one line
+print(y)
+
+def double_numbers(num):
+    return num * 2
+
+nums = [1, 2, 3, 4, 5]
+result = map(double_numbers, nums)
+result_list = list(result)
+print(result_list)
+
+result = map(lambda x: x*2, nums)
+result_list = list(result)
+print(result_list)
+
+print("\n")
